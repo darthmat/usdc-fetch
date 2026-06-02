7cd4ca09f62ead5f9f78521818fac2d80ccf6303
Mateusz Strózik <strozikmateusz@gmail.com>
2026-06-02 10:22:32 +0200

    feat: add docker with healthz controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsdcModule } from './modules/usdc/usdc.module';
+import { HealthController } from './modules/healtz/healthz.controller';
 
 @Module({
   imports: [UsdcModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
