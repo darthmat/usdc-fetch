b2e6bbc627a2242509afd60609c2da081e4089e5
Mateusz Strózik <strozikmateusz@gmail.com>
2026-06-04 13:54:16 +0200

    fix:add missing controller return type

--- src/modules/usdc/usdc.controller.ts
+++ src/modules/usdc/usdc.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { GetBlockTransfersDto } from './usdc.dto';
 import { UsdcService } from './usdc.service';
+import { UsdcTransfer } from '../blockchain/blockchain.interface';
 
 @Controller('usdc')
 export class UsdcController {
   constructor(private readonly usdcService: UsdcService) {}
 
   @Get('transfers/:blockNumber')
-  async getTransfers(@Param() params: GetBlockTransfersDto) {
+  async getTransfers(
+    @Param() params: GetBlockTransfersDto,
+  ): Promise<UsdcTransfer[]> {
     return await this.usdcService.getBlockTransfers(BigInt(params.blockNumber));
   }
 }
